f485dd12df1b87c9a91ed69accf674c3b823282a
Thomas Richard <thomrich0@gmail.com>
2024-12-05 17:02:43 +0000

    Day 3 - q 2

--- day_03/compute.py
+++ day_03/compute.py
@@ -5,13 +5,42 @@ from pathlib import Path
 from typing import Self
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(kw_only=True, frozen=True)
 class Mult:
     first: int
     second: int
+    is_active: bool
 
     def value(self) -> int:
         return self.first * self.second
+
+    @classmethod
+    def _build_activation_block(cls, data: str) -> dict[int, bool]:
+        activation_block = {0: True}
+
+        activation_idx = 0
+        while (activation_idx := data.find("do()", activation_idx)) > 0:
+            activation_block[activation_idx] = True
+            activation_idx += 4
+
+        deactivation_idx = 0
+        while (deactivation_idx := data.find("don't()", deactivation_idx)) > 0:
+            activation_block[deactivation_idx] = False
+            deactivation_idx += 7
+
+        return activation_block
+
+    @classmethod
+    def _mult_is_active(cls, offset: int, activation_block: dict[int, bool]) -> bool:
+        changes = sorted(activation_block.keys())
+
+        start = changes[0]
+        for end in changes[1:]:
+            if start < offset < end:
+                return activation_block[start]
+            start = end
+        print(f"{offset=} is_active={activation_block[start]}")
+        return activation_block[start]
 
     @classmethod
     def from_file(cls, filename: Path | str) -> list[Self]:
@@ -21,15 +50,19 @@ class Mult:
             for line in fin:
                 lines += line
 
-        pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+        activation_block = cls._build_activation_block(lines)
+        print(f"  built {activation_block=}")
+
+        op_pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
         operations = []
-        for parts in pattern.findall(lines):
-            operations.append(
-                cls(
-                    int(parts[0]),
-                    int(parts[1]),
-                )
-            )
+        start = 0
+        while (next_mul := lines.find("mul(", start)) > 0:
+            is_active = cls._mult_is_active(next_mul, activation_block)
+            if match := op_pattern.match(lines[next_mul:]):
+                operations.append(cls(first=int(match.group(1)), second=int(match.group(2)), is_active=is_active))
+
+            start = next_mul + 4
+
         return operations
 
 
@@ -37,10 +70,16 @@ def q1_lazy_mult(data: list[Mult]) -> int:
     return sum((op.value() for op in data))
 
 
+def q2_active_mult(data: list[Mult]) -> int:
+    return sum((op.value() for op in data if op.is_active))
+
+
 def main(filename: str):
     operations = Mult.from_file(filename)
     q1 = q1_lazy_mult(operations)
     print(f"Q1: lazy mult {q1}")
+    q2 = q2_active_mult(operations)
+    print(f"Q2: active mult {q2}")
 
 
 if __name__ == "__main__":
--- day_03/test_compute.py
+++ day_03/test_compute.py
@@ -2,22 +2,35 @@ from pathlib import Path
 
 import pytest
 
-from day_03.compute import q1_lazy_mult, Mult
+from day_03.compute import q1_lazy_mult, Mult, q2_active_mult
 
 
 @pytest.fixture(scope="session")
-def small_ex_txt() -> Path:
-    return Path(__file__).parent.absolute() / "small_ex.txt"
+def small_ex_txt() -> list[Mult]:
+    return Mult.from_file(Path(__file__).parent.absolute() / "small_ex.txt")
 
 
 @pytest.fixture(scope="session")
-def input_txt():
-    return Path(__file__).parent.absolute() / "input.txt"
+def small_ex_2_txt() -> list[Mult]:
+    return Mult.from_file(Path(__file__).parent.absolute() / "small_ex_2.txt")
+
+
+@pytest.fixture(scope="session")
+def input_txt() -> list[Mult]:
+    return Mult.from_file(Path(__file__).parent.absolute() / "input.txt")
 
 
 def test_q1_small_ex(small_ex_txt):
-    assert q1_lazy_mult(Mult.from_file(small_ex_txt)) == 161
+    assert q1_lazy_mult(small_ex_txt) == 161
+
+
+def test_q2_small_ex(small_ex_2_txt):
+    assert q2_active_mult(small_ex_2_txt) == 48
 
 
 def test_q1(input_txt):
-    assert q1_lazy_mult(Mult.from_file(input_txt)) == 169021493
+    assert q1_lazy_mult(input_txt) == 169021493
+
+
+def test_q2(input_txt):
+    assert q2_active_mult(input_txt) == 111762583
