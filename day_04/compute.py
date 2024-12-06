b6a8d39c02a7c217b5c18f6c62b1e95017fc4f7f
Thomas Richard <thomrich0@gmail.com>
2024-12-06 13:04:30 +0000

    Day 4 - q2

--- day_04/compute.py
+++ day_04/compute.py
@@ -65,6 +65,27 @@ class Card:
             return self.is_in_word(word, i=i + 1, pos=pos + vect, vect=vect)
         return True
 
+    def is_cross(self, word: str, *, pos: Position) -> bool:
+        assert len(word) == 3
+        cross_1 = [
+            pos + Position(-1, -1),
+            pos + Position(1, 1),
+        ]
+        cross_2 = [
+            pos + Position(1, -1),
+            pos + Position(-1, 1),
+        ]
+        first_char = word[0]
+        last_char = word[2]
+
+        for cross in (cross_1, cross_2):
+            forward = self.map.get(cross[0], "") == first_char and self.map.get(cross[1], "") == last_char
+            backward = self.map.get(cross[0], "") == last_char and self.map.get(cross[1], "") == first_char
+            if not (forward or backward):
+                return False
+
+        return True
+
     def search(self, word: str) -> list[Word]:
         all_vectors = Position.neighbours()
         found = []
@@ -74,15 +95,31 @@ class Card:
                     found.append((start_position, vect))
         return found
 
+    def search_x(self, word: str) -> list[Position]:
+        if len(word) % 2 == 0:
+            raise ValueError("Word needs to have an easy middle")
+        middle_idx = len(word) // 2
+        found = []
+        for middle_position in self.rev_map[word[middle_idx]]:
+            if self.is_cross(word, pos=middle_position):
+                found.append(middle_position)
+        return found
+
 
 def q1_find_xmas(card: Card) -> int:
     return len(card.search("XMAS"))
+
+
+def q2_find_x_mas(card: Card) -> int:
+    return len(card.search_x("MAS"))
 
 
 def main(filename: str):
     data = Card.from_file(filename)
     q1 = q1_find_xmas(data)
     print(f"Q1: {q1} XMAS")
+    q2 = q2_find_x_mas(data)
+    print(f"Q2: {q2} X-MAS")
 
 
 if __name__ == "__main__":
--- day_04/test_compute.py
+++ day_04/test_compute.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from day_04.compute import Card, q1_find_xmas
+from day_04.compute import Card, q1_find_xmas, q2_find_x_mas
 
 
 @pytest.fixture(scope="session")
@@ -19,5 +19,13 @@ def test_q1_small_ex(small_ex_txt):
     assert q1_find_xmas(small_ex_txt) == 18
 
 
+def test_q2_small(small_ex_txt):
+    assert q2_find_x_mas(small_ex_txt) == 9
+
+
 def test_q1(input_txt):
     assert q1_find_xmas(input_txt) == 2414
+
+
+def test_q2(input_txt):
+    assert q2_find_x_mas(input_txt) == 1871
