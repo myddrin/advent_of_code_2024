0a63b0eadf2803e5b138966ada4b759ef8b1c4f1
Thomas Richard <thomrich0@gmail.com>
2024-12-29 14:09:06 +0000

    WIP q2

--- day_09/compute.py
+++ day_09/compute.py
@@ -17,6 +17,9 @@ class Offset:
     def __lt__(self, other: Self) -> bool:
         return self.offset < other.offset
 
+    def __repr__(self):
+        return f"O({self.offset},{self.size})"
+
     def checksum(self, file_id: int) -> int:
         return sum((i * file_id for i in range(self.offset, self.offset + self.size)))
 
@@ -70,9 +73,22 @@ class Disk:
             if right_of < space.offset < left_of:
                 return space
 
+    def _consolidate_free_spaces(self):
+        free_spaces = sorted(self.free_spaces)
+        # free_spaces = self.free_spaces
+        self.free_spaces = [free_spaces[0]]
+        for current in free_spaces[1:]:
+            last = self.free_spaces[-1]
+            if last.last + 1 == current.offset:
+                self.free_spaces.pop(-1)
+                self.free_spaces.append(Offset(last.offset, last.size + current.size))
+            else:
+                self.free_spaces.append(current)
+
     def _fragment_to_free_space(self, file_id: int):
         this_file_map = self.file_map[file_id]
         size_unmoved = sum((off.size for off in this_file_map))
+        size_moved = 0
         original_left_most = this_file_map[0].offset
         # print(f"Fragmenting {file_id=} left_most={original_left_most} right_most={this_file_map[-1].last}")
         this_file_map = []
@@ -82,6 +98,7 @@ class Disk:
             if found.size <= size_unmoved:
                 this_file_map.append(found)
                 size_unmoved -= found.size
+                size_moved += found.size
             else:
                 # remove original one!
                 this_file_map.append(
@@ -94,14 +111,20 @@ class Disk:
                     found.offset + size_unmoved,
                     found.size - size_unmoved,
                 )
+                size_moved += size_unmoved
                 size_unmoved = 0
                 self.free_spaces.insert(0, left_free)
+                # self.free_spaces.append(left_free)
 
         if size_unmoved:
             this_file_map.append(Offset(original_left_most, size_unmoved))
+            self.free_spaces.append(Offset(original_left_most + size_moved, size_moved))
+        else:
+            self.free_spaces.append(Offset(original_left_most, size_moved))
+
         self.file_map[file_id] = this_file_map
-        # this_file_map = self.file_map[file_id]
-        print(f"  -> {file_id=} left_most={this_file_map[0].offset} right_most={this_file_map[-1].last}")
+        # self.free_spaces = sorted(self.free_spaces)
+        self._consolidate_free_spaces()
 
     def _move_to_free_space(self, file_id: int):
         assert len(self.file_map[file_id]) == 1, "lazy logic"
@@ -123,63 +146,90 @@ class Disk:
                 self.file_map[file_id] = [new_file]
                 if new_free.size > 0:
                     self.free_spaces.insert(0, new_free)
-                print(f"  -> {file_id=} moved from left_most={left_most}..{new_file.offset}")
-                return
+                    # self.free_spaces.append(new_free)
+                # print(f"  -> {file_id=} moved from left_most={left_most}..{new_file.offset}")
+                self.free_spaces.append(Offset(left_most, size))
+                break
             else:
                 last_found = found.offset
 
-        if found is None:
-            print(f"  -> {file_id=} does not move")
+        # self.free_spaces = sorted(self.free_spaces)
+        self._consolidate_free_spaces()
+        # if found is None:
+        #     print(f"  -> {file_id=} does not move")
 
     def compress(self) -> Self:
         obj = deepcopy(self)
+        print("Compressing disk")
+        if (n_files := len(obj.file_map)) > 100:
+            n_files = len(obj.file_map) // 100
         for file_id in sorted(obj.file_map.keys(), reverse=True):
             obj._fragment_to_free_space(file_id)
+            if file_id % n_files == (n_files - 1):
+                print(".", end="", flush=True)
+        print("")
         return obj
 
     def defragment(self) -> Self:
         obj = deepcopy(self)
+        print("Defragmenting disk")
+        if (n_files := len(obj.file_map)) > 100:
+            n_files = len(obj.file_map) // 100
         for file_id in sorted(obj.file_map.keys(), reverse=True):
             obj._move_to_free_space(file_id)
+            if file_id % n_files == (n_files - 1):
+                print(".", end="", flush=True)
+        print("")
         return obj
 
     def checksum(self) -> int:
         return sum((sum((off.checksum(file_id) for off in offset)) for file_id, offset in self.file_map.items()))
 
+    def _size_to_str(self, file_id: int | None, size: int) -> str:
+        half = size // 2
+        if file_id is None:
+            file_str = "----"
+        else:
+            file_str = f"{file_id:04d}"
+        return "".join(["____"] * half) + file_str + "".join(["____"] * (size - half))
+
     def to_file(self, filename: Path | str):
         print(f"Writing {filename}")
         with open(filename, "w") as fout:
-            disk_map = ["." for _ in range(self.disk_size)]
+            disk_map = {}
+            fout.write(f"free={self.free_size} file={self.file_size} total={self.disk_size}\n")
+            fout.write("|")
+            for offset in self.free_spaces:
+                disk_map[offset.offset] = self._size_to_str(None, offset.size)
             for file_id, offsets in self.file_map.items():
                 for off in offsets:
-                    for i in range(off.offset, off.last + 1):
-                        disk_map[i] = f"{file_id:04d}"
-            fout.write(",".join(disk_map) + "\n")
+                    disk_map[off.offset] = self._size_to_str(file_id, off.size)
+            for off in sorted(disk_map.keys()):
+                fout.write(disk_map[off] + "|")
+            fout.write("\n")
 
 
-def main(filename: str, output_base: str | None):
-    if output_base and not output_base.endswith(".txt"):
-        output_base += ".txt"
+def main(filename: str, output: bool):
     disk = Disk.from_file(filename)
-    if output_base:
-        disk.to_file(f"initial_{output_base}")
+    if output:
+        disk.to_file("d9_initial.txt")
     q1_disk = disk.compress()
     q1 = q1_disk.checksum()
     print(f"Q1: checksum after fragmentation is {q1}")
-    if output_base:
-        q1_disk.to_file(f"q1_{output_base}")
+    if output:
+        q1_disk.to_file("d9_q1.txt")
     q2_disk = disk.defragment()
     q2 = q2_disk.checksum()
     print(f"Q2: checksum after defragmentation is {q2}")
-    if output_base:
-        q2_disk.to_file(f"q2_{output_base}")
+    if output:
+        q2_disk.to_file("d9_q2.txt")
 
 
 if __name__ == "__main__":
     fd = Path(__file__).parent.absolute() / "input.txt"
     parser = ArgumentParser()
     parser.add_argument("--input", type=str, default=str(fd), help="Input file")
-    parser.add_argument("--output", type=str, help="Base output filename")
+    parser.add_argument("--output", action="store_true", help="Output steps")
     args = parser.parse_args()
 
     main(args.input, args.output)
--- day_09/test_compute.py
+++ day_09/test_compute.py
@@ -15,21 +15,45 @@ def input_txt() -> Disk:
     return Disk.from_file(Path(__file__).parent.absolute() / "input.txt")
 
 
+def test_load_small_ex(small_ex_txt):
+    assert small_ex_txt.file_size == 28
+    assert small_ex_txt.free_size == 14
+    assert small_ex_txt.disk_size == 42
+
+
+def test_load_input(input_txt):
+    assert input_txt.file_size + input_txt.free_size == input_txt.disk_size
+
+
 def test_q1_small(small_ex_txt):
     disk = small_ex_txt.compress()
+    assert disk is not small_ex_txt
+    assert disk.file_size == small_ex_txt.file_size
+    assert disk.free_size == small_ex_txt.free_size
     assert disk.checksum() == 1928
+    assert sorted(disk.free_spaces) == disk.free_spaces
 
 
 def test_q2_small(small_ex_txt):
     disk = small_ex_txt.defragment()
+    assert disk is not small_ex_txt
+    assert disk.file_size == small_ex_txt.file_size
+    assert disk.free_size == small_ex_txt.free_size
     assert disk.checksum() == 2858
+    assert sorted(disk.free_spaces) == disk.free_spaces
 
 
 def test_q1(input_txt):
     disk = input_txt.compress()
+    assert disk.file_size + disk.free_size == disk.disk_size
+    assert disk.file_size == input_txt.file_size
+    assert disk.free_size == input_txt.free_size
     assert disk.checksum() == 6216544403458
 
 
 def test_q2(input_txt):
     disk = input_txt.defragment()
-    assert disk.checksum() < 7912125338026
+    assert disk.file_size + disk.free_size == disk.disk_size
+    assert disk.file_size == input_txt.file_size
+    assert disk.free_size == input_txt.free_size
+    assert disk.checksum() < 6299512391513
